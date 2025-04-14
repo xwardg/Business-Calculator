cdeea12ea01d60430e18b1712186d88314652f3d
Scott Ward <gray.ward@icloud.com>
2025-04-13 22:30:46 -0400

    Updated business calculator logic and form

--- app.py
+++ app.py
@@ -8,21 +8,26 @@ def calculator():
     if request.method == "POST":
         try:
             materialcost = float(request.form["materialcost"])
+            matpercent1 = float(request.form["matpercent1"])
+            jhourly = float(request.form["jhourly"])
+            jpd = float(request.form["jpd"])
             Jhours = float(request.form["Jhours"])
             Jdays = float(request.form["Jdays"])
             Whours = float(request.form["Whours"])
+            wpd = float(request.form["wpd"])
 
-            Jhours2 = (Jhours - Jdays)
-            matcostfinal = (materialcost * 1.1)
-            totalcost = (Jhours2 * 45) + (Jdays * 65) + (Whours * 35) + matcostfinal
+            matpercent2 = matpercent1 / 100
+            Jhours2 = Jhours - Jdays
+            matcostfinal = materialcost * (1 + matpercent2)
+
+            totalcost = (Jhours2 * jhourly) + (Jdays * (jhourly + jpd)) + (Whours * 35) + matcostfinal + wpd
         except:
-            totalcost = "Invalid input. Please enter numbers only."
+            totalcost = "Invalid input. Please enter valid numbers."
 
     return render_template("calculator.html", totalcost=totalcost)
 
 if __name__ == "__main__":
     import os
-
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
 
